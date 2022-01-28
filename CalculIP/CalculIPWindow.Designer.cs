64d4959f09b047c6db31a6585a384d75c747b92e
Adrien Couturier <adrien.couturier@gmail.com>
2022-01-28 10:53:44 -0500

    MAJ Cfm

--- CalculIP/CalculIPWindow.Designer.cs
+++ CalculIP/CalculIPWindow.Designer.cs
@@ -296,11 +296,10 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::CalculIP.Properties.Resources.Conformit;
-            this.pictureBox1.Location = new System.Drawing.Point(5, 4);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(56, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(323, 98);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.Size = new System.Drawing.Size(227, 75);
             this.pictureBox1.TabIndex = 10;
             this.pictureBox1.TabStop = false;
             // 
